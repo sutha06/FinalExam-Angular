a1d4a048ec7a13a2025eabf1a64443de934cfc6d
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-16 09:15:50 -0400

    init commit

---
+++ src/app/pet.ts
@@ -0,0 +1,11 @@
+export class Pet {
+  public id : number = 0;
+  public name : string = '';
+  public petKind : string = '';
+  public breed : string = '';
+
+  public petFirstName : string = '';
+  public petLastName : string = '';
+
+  public picture : string = '';
+}
