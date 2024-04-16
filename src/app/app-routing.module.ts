88b866da5678a0dfc2b5b89f3369065d787ceb1d
sutha06 <suthakaran.siva0601@gmail.com>
2024-04-16 09:49:33 -0400

    app routing module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PetDetailsComponent} from "./pet-details/pet-details.component";
 
 const routes: Routes = [
-
+  {path: '', component: PetDetailsComponent},
+  {path: '**', redirectTo: ''},
 ];
 
 @NgModule({
